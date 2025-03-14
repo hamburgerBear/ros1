955bea43c2734c7229d63340aff91673c9d29e8c
gaojie <gaojie@yjh.com>
2025-03-14 13:35:06 +0800

    test

--- src/modules/control/control_plugin/follow_wall_controller/src/state_approach_wall.cpp
+++ src/modules/control/control_plugin/follow_wall_controller/src/state_approach_wall.cpp
@@ -2,18 +2,14 @@
 
 namespace control {
 
-/*
-  1. 从Ownner中获取传感器资源，以及一些针对该控制器的(Root)通用算法
-  2. 从OnExit传入任务目标(Action goal)
-*/
 void StateApproachWall::OnEnter() {
   ROS_INFO("StateApproachWall::OnEnter");
   control_ = std::make_unique<Arc>("arc", Owner().injector_);
 
   std::vector<Eigen::Vector3d> path;
-  path.emplace_back(toRad(80.0), 0.0, 0.0);
+  path.emplace_back(toRad(70.0), 0.0, 0.0);
   std::shared_ptr<Arc::ArcArgs> args =
-      std::make_shared<Arc::ArcArgs>(path, 0.1, -0.15);
+      std::make_shared<Arc::ArcArgs>(path, 0.1, 0.15 * Owner().getFollowDir());
   control_->setGoal(args);
 }
 
@@ -23,11 +19,11 @@ void StateApproachWall::OnExit() {}
 
 Transition StateApproachWall::GetTransition() {
   if (control_->isFinish() || control_->isFail())
-    return SiblingTransition<StateForward>(0.02);
+    return SiblingTransition<StateForward>(0.04);
   else if (isFrontSideApproach())
     return SiblingTransition<StateFollowWall>();
   else if (isSideApproach())
-    return SiblingTransition<StateForward>(0.02);
+    return SiblingTransition<StateForward>(0.025);
   else
     return NoTransition();
 }
